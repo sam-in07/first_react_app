43de592d52e957cec5a94053db1ef0ad4e30c07c
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2025-12-03 21:18:06 +0600

    styles

--- src/app.jsx
+++ src/app.jsx
@@ -9,7 +9,15 @@ import './app.css'
 
 const Card = ( {title }) => {
   return(
-    <div>
+    
+    <div className='card' style={{
+      border: '1px solid #4b5362', 
+      padding: '20px',
+      margin: '10px',
+      backgroundColor : '#31363f',
+      borderRadius: '10px',
+      minHeight: '100px',        
+    }} >
       <h2>
          {title }
       </h2>
@@ -19,12 +27,14 @@ const Card = ( {title }) => {
 
 const App = () => {
   return (
-    <div>
-      <h2>Func Arrow Compoen</h2>
+    <div className="card-container">
 
-      <Card title="Samin boka"/>  
+      {/* <h2>Func Arrow Compoen</h2> */}
+    
+     <Card title="Samin boka"/>  
       <Card title="Samin valona" /> 
       <Card title="Samin ki pare na" /> 
+      
     </div> 
   )
 }
