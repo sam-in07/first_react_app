2e4db99bfdbde062983974f5e7ef252d6d2eb409
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2025-12-03 20:17:21 +0600

    components

--- src/app.jsx
+++ src/app.jsx
@@ -3,41 +3,31 @@ import preactLogo from './assets/preact.svg'
 import viteLogo from '/vite.svg'
 import './app.css'
 
-export function App() {
-  const [count, setCount] = useState(0)
+// function App() {
+  
+// }
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+const Card = () => {
+  return(
+    <div>
+      <h2>
+        Card Component
+      </h2>
+    </div>
   )
 }
+
+const App = () => {
+  return (
+    <div>
+      <h2>Func Arrow Compoen</h2>
+
+      <Card /> 
+      <Card /> 
+      <Card /> 
+    </div> 
+  )
+}
+
+
+export default App
