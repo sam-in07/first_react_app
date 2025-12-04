75b6c77a009808f72676858eb29bb77096cab0fe
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2025-12-04 21:11:10 +0600

    header Completet

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 HEllow
-![alt text](public\readme\image.png)
 
-![alt text](public\readme\image-1.png)
-
-![alt text](public\readme\image-2.png)
 
 [HOoks DOC](https://react.dev/reference/react/hooks)
 
 [TailwindVIte](https://tailwindcss.com/docs/installation/using-vite)
+
+
+[APi Layer](https://github.com/public-apis/public-apis)
+
+[Movie api](https://developer.themoviedb.org/reference)
+
+[Discover Movie api](https://developer.themoviedb.org/reference/discover-movie)
+
+
+
+![alt text](public\readme\image.png)
+
+
+
+
+
+
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const Search = ({ searchTerm, setSearchTerm }) => {
+  //the current value of the input   a function that updates the value
   return (
     <div className="search">
       <div>
@@ -11,10 +12,36 @@ const Search = ({ searchTerm, setSearchTerm }) => {
           type="text"
           placeholder="Search through thousands of movies"
           value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          onChange={(event) => setSearchTerm(event.target.value)}
         />
       </div>
     </div>
   )
 }
 export default Search
+
+
+
+/*
+value={searchTerm}
+
+The visible text in the input is controlled by the searchTerm state coming from the parent.
+
+This makes it a controlled input.
+
+onChange={(e) => setSearchTerm(e.target.value)}
+
+Whenever the user types:
+
+React receives the typed value (e.target.value)
+
+Calls setSearchTerm(...)
+
+Updates the state in the parent component
+
+Causes a re-render
+
+Input shows the new value
+
+This is how controlled forms work in React.
+*/
