f3fe71ce07655d9e8d5ac23f6842917f4a1459c0
Samin Ahmed Chowdhury <samin.ahmedchowdhury@gmail.com>
2025-12-04 17:53:19 +0600

    Refactor Card component for improved structure and readability

--- src/app.jsx
+++ src/app.jsx
@@ -1,56 +1,50 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
-import{useEffect, useState} from 'react'
+import { useState } from "react";
+import preactLogo from "./assets/preact.svg";
+import viteLogo from "/vite.svg";
+import "./app.css";
+import { useEffect, useState } from "react";
 // function App() {
-  
+
 // }
 
-const Card = ( {title }) => {
-   const [hasliked, setHasLiked] = useState(false);
+const Card = ({ title }) => {
+  const [count, setCount] = useState(0); 
+  const [hasliked, setHasLiked] = useState(false);
 
-   useEffect(() => {
-    console.log('like state changed ', hasliked);
-   });
-  return(
-    
-    <div className='card' style={{
-      border: '1px solid #4b5362', 
-      padding: '20px',
-      margin: '10px',
-      backgroundColor : '#31363f',
-      borderRadius: '10px',
-      minHeight: '100px',        
-    }} >
-      <h2>
-         {title }
-      </h2>
+  useEffect(() => {
+    console.log("like state changed ", hasliked);
+  });
+  
+  return (
+    <div
+      className="card" onClick={() => setCount(
+        count+1
+      )}
+     
+    >
+
+      <h2>{title}  <br /> {count || null} </h2>
       <button onClick={() => setHasLiked(!hasliked)}>
-        //chnages if liked or not 
-        {
-          hasliked ? 'liked' : 'none' 
-        } 
+        //chnages if liked or not
+        {hasliked ? "liked" : "none"}
       </button>
     </div>
-  )
-}
+  );
+};
 
 const App = () => {
   const [hasliked, setHasLiked] = useState(false);
   //varibalename  set varibale
   return (
     <div className="card-container">
-
       {/* <h2>Func Arrow Compoen</h2> */}
-    
-     <Card title="Samin boka"/>  
-      <Card title="Samin valona" /> 
-      <Card title="Samin ki pare na" /> 
-      
-    </div> 
-  )
-}
+
+      <Card title="Samin boka" />
+      <Card title="Samin valona" />
+      <Card title="Samin ki pare na" />
+    </div>
+  );
+};
 
 
-export default App
+export default App;
